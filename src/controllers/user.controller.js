365cd92bbf148e9810d8844a0a6d4311060849b0
Kaue Felipe Pereira <kauefelipemail@gmail.com>
2023-01-31 12:27:55 -0300

    refactor: config route create user config

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,19 @@ const create = (req, res) => {
   const { name, username, email, password, avatar, background } = req.body;
   //const user = req.body;
   if (!name || !username || !email || !password || !avatar || !background){
-    res.json("Submit all fields to registration.");
-  }
-  res.json("ok");
+    //res.json("Submit all fields to registration.");
+    res.status(400).send({message :"Please fill all fields"})
+  };
+  res.status(201).send({
+    message:"User created successfuly",
+    user:{
+        name,
+        username,
+        email,
+        avatar,
+        background,
+    },
+  });
 };
 
 module.exports = { create };
