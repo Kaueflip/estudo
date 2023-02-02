8374f77567e16bb10ee0a88755f72cc0f08b9487
Kaue Felipe Pereira <kauefelipemail@gmail.com>
2023-02-02 10:22:39 -0300

    user service update

--- src/services/user.service .js
+++ src/services/user.service .js
@@ -2,10 +2,9 @@ const { userInfo } = require('os');
 const User = require('../models/User');
 
 const createService = (body) => User.create(body);
-
 const findAllService = () => User.find();
 
 module.exports = {
     createService,
-    findAllService,
+    findAllService
 };
